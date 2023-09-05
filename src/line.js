0cb182d3b506042dcb44ef7e7dbc73d370082494
Rabia Çelik <rabia.celk150@gmail.com>
2023-09-05 10:25:57 +0300

    initial

---
+++ src/line.js
@@ -0,0 +1,13 @@
+var lineJson = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"coordinates":[[35.18568555164347,39.06731647398712],[34.61994723679902,39.18308077751507],[12.585518158885577,42.110732936856465],[14.3732524126319,47.511455527479114]],"type":"LineString"}}]}
+
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }}
